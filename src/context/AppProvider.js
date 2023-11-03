166e201cb0514128a86265da7ceaed52ff887db5
surajverma2587 <surajverma2587@gmail.com>
2023-11-03 19:58:01 +0000

    add reducer

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,9 +1,16 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { basketReducer } from "../reducers/basketReducer";
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const value = {};
+  const initialState = {
+    basket: [],
+  };
+
+  const [state, dispatch] = useReducer(basketReducer, initialState);
+
+  const value = { state, dispatch };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
---
+++ src/reducers/basketReducer.js
@@ -0,0 +1,3 @@
+export const basketReducer = (state, action) => {
+  return state;
+};
