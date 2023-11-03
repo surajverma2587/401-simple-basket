44797bb09ba7d360ec568ba24f36195ed4f30512
surajverma2587 <surajverma2587@gmail.com>
2023-11-03 19:40:11 +0000

    basic app

---
+++ src/AppRoutes.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { Checkout } from "./pages/Checkout";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  );
+};
